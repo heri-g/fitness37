9a1760ed396d5b0e16a7f3f51a950b542c07aced
heri-g <herigutierrez@Heris-MacBook-Pro.local>
2017-01-07 18:46:36 -0500

    add all

---
+++ src/app/front-page/front-page.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'front-page',
+  templateUrl: './front-page.component.html',
+  styleUrls: ['./front-page.component.css']
+})
+export class FrontPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    // let hey = document.getElementById('left-col');
+    // hey.transition('horizontal flip', '500ms');
+    // console.log(hey);
+  }
+}
